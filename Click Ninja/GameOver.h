5fe2103f84c38ee86a38245d109f9e4cefa99691
Michael Jaoudi <mjaoudi93@gmail.com>
2012-07-09 00:26:22 -0400

    Added Timer and Game Over Scene

---
+++ Click Ninja/GameOver.h
@@ -0,0 +1,16 @@
+//
+//  GameOver.h
+//  Click Ninja
+//
+//  Created by Michael Jaoudi on 7/9/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameOver : CCLayer {
+    
+}
+
+@end
---
+++ Click Ninja/GameOver.m
@@ -0,0 +1,14 @@
+//
+//  GameOver.m
+//  Click Ninja
+//
+//  Created by Michael Jaoudi on 7/9/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameOver.h"
+
+
+@implementation GameOver
+
+@end
